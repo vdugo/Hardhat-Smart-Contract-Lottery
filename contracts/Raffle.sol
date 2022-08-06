f20e2b60fd5116ee16a8afb51eab3c3229002db4
Vincent <v3rdugo@yahoo.com>
2022-08-05 17:19:03 -0700

    successful compilation

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -14,7 +14,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle
+contract Raffle is VRFConsumerBaseV2
 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
@@ -23,16 +23,16 @@ contract Raffle
     /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee)
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2)
     {   
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle()
+    function enterRaffle() public payable
     {
         if (msg.value < i_entranceFee)
         {
-            revert Raffle__NotEnoughETHEntered;
+            revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
 
@@ -46,18 +46,18 @@ contract Raffle
         // 2 transaction process
     }
 
-    function fulfillRandomWords() internal override
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override
     {
 
     }
 
     function getEntranceFee() public view returns(uint256)
     {
-        return i_entranceFee
+        return i_entranceFee;
     }
 
     function getPlayer(uint256 index) public view returns(address)
     {
-        return s_players[index]
+        return s_players[index];
     }
 }
