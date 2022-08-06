972bce28f8104d689e69fb005a70d30968d49cd8
Vincent <v3rdugo@yahoo.com>
2022-08-05 17:04:59 -0700

    import chainlink contracts

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,27 +9,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle__NotEnoughETHEntered();
+
 contract Raffle
 {
+    /* State Variables */
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEnter(address indexed player);
 
     constructor(uint256 entranceFee)
-    {
+    {   
         i_entranceFee = entranceFee;
     }
 
     function enterRaffle()
     {
+        if (msg.value < i_entranceFee)
+        {
+            revert Raffle__NotEnoughETHEntered;
+        }
+        s_players.push(payable(msg.sender));
 
+        emit RaffleEnter(msg.sender);
     }
 
-    function pickRandomWinner()
+    function requestRandomWinner() external 
     {
-         
+        // request the random number
+        // once we get it, do something with it
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords() internal override
+    {
+
     }
 
     function getEntranceFee() public view returns(uint256)
     {
         return i_entranceFee
+    }
+
+    function getPlayer(uint256 index) public view returns(address)
+    {
+        return s_players[index]
     }
 }
